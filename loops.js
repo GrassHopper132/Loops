f5a0494e354345dd31a438692ceb00aa70534791
GrassHopper132 <crhymer101@gmail.com>
2026-07-09 19:46:53 -0400

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,4 +1,20 @@
+// fizzz buz
+console.log("Part 1: Fizz Buzz");
 
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+  } 
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  } 
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  } 
+  else {
+    console.log(i);
+  }
+}
 
 
 
